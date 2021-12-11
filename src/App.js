9f1fe7af1af6d6517908ae1be95443c96c2501e4
Web-Dev-Art <my.samsung.wa@gmail.com>
2021-12-11 12:41:09 +0500

    Completed: 03 React Basics & Working With Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Expenses from './components/Expenses';
+
 import './App.css';
 
+
 function App() {
+  const DUMMY_EXPENSES = [
+      {id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date('August 14, 2020')},
+      {id: 'e2', title: 'New Tv', amount: 799.49, date: new Date('March 12, 2021')},
+      {id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date('March 28, 2021')},
+      {id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date('June 12, 2021')}
+  ];
+
+
   return (
     <div className="App">
       <h2>Let's get Started</h2>
+      <Expenses items={DUMMY_EXPENSES} />
     </div>
   );
 }
